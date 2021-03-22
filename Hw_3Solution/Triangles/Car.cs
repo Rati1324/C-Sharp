9e06eb99c7ca1b9223ee5afbbf9e341e3377c82a
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-03-22 23:41:05 +0400

    finished

---
+++ Hw_3Solution/Triangles/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw_3
+{
+    // amocana 6.4.3
+    public class Car
+    {
+        public static int CalcDis(int Capacity, int Distance)
+            => Distance * Capacity;
+
+        public static float CalcDis(float Speed, float Time)
+            => Speed * Time;
+    }
+}
