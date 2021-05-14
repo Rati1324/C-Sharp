8049dec56655afb9d660e6fcb77d402f1d82397a
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-05-14 18:31:18 +0400

    linq

---
+++ hw8/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            XElement elem = XElement.Load("Students.xml");
+            var students = elem.Elements("Student");
+            var names = students.Elements("Name");
+           
+            Console.WriteLine("names of students:");
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item.Element("First").Value + " " + item.Element("Last").Value);
+            }
+
+            Console.WriteLine("\nstudents who study applied math");
+
+            var Math = from y in students
+                    where (string)y.Element("Course") == "Applied Math"
+                    select y.Element("Name");
+            foreach (var item in Math)
+            {
+                Console.WriteLine(item.Element("First").Value + " " + item.Element("Last").Value);
+            }
+
+            Console.WriteLine("\nstudents who live in london");
+            var London = students.Where(y => (string)y.Element("Address").Element("City") == "London").ToList();
+
+            foreach (var item in London)
+            {
+                Console.WriteLine(item.Element("Name").Element("First").Value + " " + item.Element("Name").Element("Last").Value);
+            }
+
+            Console.WriteLine("\nhow many students are from london");
+            var LondonNum = (from y in students.Descendants("City")
+                            where y.Value == "London"
+                            select y).Count();
+            Console.WriteLine(LondonNum);
+        
+
+        }
+    }
+}
