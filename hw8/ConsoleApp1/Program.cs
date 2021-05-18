46ff1262a79660ccf9c79d23ee407933af50ba36
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-05-18 12:14:05 +0400

    _

--- hw8/ConsoleApp1/Program.cs
+++ hw8/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -12,50 +13,82 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-            XElement elem = XElement.Load("Students.xml");
-            var students = elem.Elements("Student");
-            var names = students.Elements("Name");
-           
-            Console.WriteLine("names of students:");
-
-            foreach (var item in names)
+            #region classes and xml
+            List<Student> S = new List<Student>()
             {
-                Console.WriteLine(item.Element("First").Value + " " + item.Element("Last").Value);
-            }
+                new Student("02030104052", "John", "Johnson", "Math", "City1", "Street2", "Male"){GPA = 2.3F },
+                new Student("01022342451", "Nick", "Nickson", "History", "City4", "Street1", "Male"){GPA = 1.3F },
+                new Student("14234234256", "Kate", "Kateson", "English", "City53", "Street14", "Female"){GPA = 4.1F },
+                new Student("34353233425", "Nick", "Johnson", "History", "City32", "Street10", "Male"){GPA = 1.3F },
+                new Student("10292029282", "Mari", "Maridze", "Physics", "City5", "Street10", "Female"){GPA = 3.5F },
+                new Student("34234324233", "Nino", "Ninodze", "Physics", "City6", "Street4", "Female"){GPA = 2.8F },
+                new Student("20203023023", "Nana", "Nanadze", "Math", "City6", "Street5", "Male"){GPA = 3.7F }
+            };
 
-            Console.WriteLine("\nstudents who study applied math");
+            XDocument xDoc = new XDocument(
+                    new XDeclaration("1.0", "UTF-16", null),
+                    new XElement("Students",
+                        (from s in S
+                         select new XElement("Student",
+                            new XElement("id", s.Id),
+                            new XElement("Name",
+                                new XElement("First", s.F_name),
+                                new XElement("Last", s.L_name)),
+                            new XElement("Major", s.Major),
 
-            var Math = from y in students
-                    where (string)y.Element("Course") == "Applied Math"
-                    select y.Element("Name");
-            foreach (var item in Math)
-            {
-                Console.WriteLine(item.Element("First").Value + " " + item.Element("Last").Value);
-            }
+                            new XElement("Address",
+                                new XElement("City", s.City),
+                                new XElement("Street", s.Street)),
+                            new XElement("Gender", s.Gender),
+                            new XElement("GPA", s.GPA)
+                    ))));
 
-            Console.WriteLine("\nstudents who live in london");
-            var London = students.Where(y => (string)y.Element("Address").Element("City") == "London").ToList();
+            StringWriter sw = new StringWriter();
+            XmlWriter xWrite = XmlWriter.Create(sw);
+            xDoc.Save("C:/Users/rati/source/repos/C-Sharp/hw8/ConsoleApp1/bin/Debug/Students.xml");
+            #endregion
 
-            foreach (var item in London)
-            {
-                Console.WriteLine(item.Element("Name").Element("First").Value + " " + item.Element("Name").Element("Last").Value);
-            }
+            var doc = XElement.Load("Students.xml");
+            var Students = doc.Elements();
 
-            Console.WriteLine("\nhow many students are from london");
-            var LondonNum = (from y in students.Descendants("City")
-                            where y.Value == "London"
-                            select y).Count();
-            Console.WriteLine(LondonNum);
+            var Majors = from x in Students
+                         select x.Descendants("First").First().Value + " " + x.Descendants("Last").First().Value
+                         + " " + x.Element("Major").Value;
 
-            Console.WriteLine("\nstudents ordered by GPA");
-            var GPA = students.OrderBy(y => (string)y.Element("GPA")).Reverse();
-            foreach (var item in GPA)
-            {
-                Console.WriteLine((string)item.Element("Name").Element("First") + " " + (string)item.Element("Name").Element("Last") + " " +item.Element("GPA").Value);
-            }
+            Console.WriteLine("Who studies what");
+            foreach (var item in Majors) Console.WriteLine(item);
+
+            var MathMajors = from x in Students
+                             where x.Element("Major").Value == "Math"
+                             select x.Descendants("First").First().Value + " " + x.Descendants("Last").First().Value;
+            Console.WriteLine("\nStudents who study Math");
+            foreach (var item in MathMajors) Console.WriteLine(item);
+
+
+            var ByGpa = Students.OrderBy(y => y.Element("GPA").Value);
+            Console.WriteLine("\nOrdered by GPA");
+            foreach (var item in ByGpa) Console.WriteLine(item.Descendants("First").First().Value + " " + item.Descendants("Last").First().Value + " " + item.Element("GPA").Value);
+
+            var City6 = Students.Count(y => y.Descendants("City").First().Value == "City6");
+            Console.WriteLine($"\nHow many of them live in City6\n{City6}");
+
+            var All = from x in Students
+                      select x.Descendants("First").First().Value + " " + x.Descendants("Last").First().Value;
+            Console.WriteLine("\nList of all students");
+            foreach (var item in All){ Console.WriteLine(item);}
+
+        }
         
+        public static void UpdateGpa(float GPA, string id)
+        {
+            XDocument Doc = XDocument.Load("Students.xml");
 
+            foreach (var item in Doc.Descendants("Student"))
+            {
+                if (item.Element("id").Value == id)
+                    item.Element("GPA").Value = GPA.ToString();
+            }
+            Doc.Save("C:/Users/rati/source/repos/C-Sharp/hw8/ConsoleApp1/bin/Debug/Students.xml");
         }
     }
 }
---
+++ hw8/ConsoleApp1/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        private string id;
+        private string f_name;
+        private string l_name;
+        private float gpa;
+        private string major;
+        private string city;
+        private string street; 
+        private string gender;
+
+        public string F_name { get => f_name; set => f_name = value; }
+        public string L_name { get => l_name; set => l_name = value; }
+        public float GPA { get => gpa; set => gpa = value; }
+        public string Major{ get => major; set => major = value; }
+        public string City{ get => city; set => city = value; }
+        public string Street { get => street; set => street = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public string Id { get => id; set => id = value; }
+        public Student(string id, string f_name, string l_name, string major, string city, string street, string gender)
+        {
+            this.id = id;
+            this.f_name = f_name;
+            this.l_name = l_name;
+            this.major = major;
+            this.city = city;
+            this.street = street;
+            this.gender = gender;
+        }
+
+
+    }
+}
